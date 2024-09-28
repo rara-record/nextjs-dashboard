4fb730ef6be10782ca49788884fab82065d5ac19
BORA <a52447879@gmail.com>
2024-09-28 18:26:52 +0900

    feat: 차트와 최근 송장 유저 렌더링

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,23 @@
-export default function page() {
-  return <div>Dashboard</div>;
+import { lusitana } from 'app/ui/fonts';
+import { fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+
+export default async function page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+
+  return (
+    <main>
+      <h1
+        className={`${lusitana.className} mb-4 text-xl md:text-2xl`}
+      >
+        Dashboard
+      </h1>
+      <div className="mt-6 grid grid-cols-1 gap6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -9,7 +9,7 @@ export const formatCurrency = (amount: number) => {
 
 export const formatDateToLocal = (
   dateStr: string,
-  locale: string = 'en-US',
+  locale: string = 'en-US'
 ) => {
   const date = new Date(dateStr);
   const options: Intl.DateTimeFormatOptions = {
@@ -21,21 +21,37 @@ export const formatDateToLocal = (
   return formatter.format(date);
 };
 
+/**
+ *
+ * @param revenue 각 월의 매출 정보
+ * @returns
+ */
 export const generateYAxis = (revenue: Revenue[]) => {
-  // Calculate what labels we need to display on the y-axis
-  // based on highest record and in 1000s
+  // 그래프의 Y축을 동적으로 생성
+  // 매출 데이터를 기반으로 차트의 Y축 레이블을 생성합니다.
+  // 가장 높은 매출액을 기준으로 적절한 눈금을 만듭니다.
+
   const yAxisLabels = [];
-  const highestRecord = Math.max(...revenue.map((month) => month.revenue));
-  const topLabel = Math.ceil(highestRecord / 1000) * 1000;
+
+  // 가장 높은 매출액 찾기
+  const highestRecord = Math.max(
+    ...revenue.map((month) => month.revenue)
+  );
+
+  // 가장 높은 매출을 1000 단위로 올림
+  const topLabel = Math.ceil(highestRecord / 1000) * 1000; // 5000
 
   for (let i = topLabel; i >= 0; i -= 1000) {
     yAxisLabels.push(`$${i / 1000}K`);
-  }
+  } // [ '$5K', '$4K', '$3K', '$2K', '$1K', '$0K' ] yAxisLabels
 
   return { yAxisLabels, topLabel };
 };
 
-export const generatePagination = (currentPage: number, totalPages: number) => {
+export const generatePagination = (
+  currentPage: number,
+  totalPages: number
+) => {
   // If the total number of pages is 7 or less,
   // display all pages without any ellipsis.
   if (totalPages <= 7) {
