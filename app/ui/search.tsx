375eabdf1f1d3799bfd6405ffc2c891dbdc28ef5
BORA <a52447879@gmail.com>
2024-09-28 21:07:23 +0900

    feat: 검색결과를 URL에 업데이트하고 페이지를 새로고침하면 이전 검색어가 유지되게끔 합니다.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,41 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from 'next/navigation';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+// defaultValue vs. value / Controlled vs. Uncontrolled
+//상태를 사용하여 입력 값을 관리하는 경우 value 속성을 ​​사용하여 이를 제어되는 구성 요소로 만듭니다.
+// 이는 React가 입력 상태를 관리한다는 의미입니다.
+//그러나 상태를 사용하지 않으므로 defaultValue 을 사용할 수 있습니다.
+// 이는 기본 입력이 자체 상태를 관리한다는 의미입니다.상태 대신 URL에 검색어를 저장하므로 괜찮습니다.
+
+export default function Search({
+  placeholder,
+}: {
+  placeholder: string;
+}) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    // replace는 현재 URL을 새로운 URL로 교체합니다. 이 경우, 검색 쿼리를 URL에 반영하여 현재 페이지의 상태를 URL에 동기화합니다.
+    replace(`${pathname}?${params.toString()}`);
+
+    // pathname : /dashboard/invoices
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +44,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        // 페이지를 새로고침하면 이전 검색어가 유지됩니다.
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
