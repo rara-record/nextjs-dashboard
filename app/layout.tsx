70bf14f8b67d1813529e00b0bb64c8056ca8e344
BORA <a52447879@gmail.com>
2024-09-28 14:34:44 +0900

    feat: font 추가하기

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+// antialiased: 글꼴을 부드럽게 만듦
 
 export default function RootLayout({
   children,
@@ -7,7 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
